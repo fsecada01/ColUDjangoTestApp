5ad07224606b4ea59f0d4a72ac898aa9baf3dad8
Francis Secada <francis.secada@gmail.com>
2019-07-21 14:55:24 -0400

    Small edits to README.md file

--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from main import views
-
-urlpatterns = [
-  url(r'^admin/', admin.site.urls),
-  url(r'^$', views.home, name='home'),
-]
